0a808085f23ea9259a0bf8e5c52dd21d98cc6011
Andrei Bechet <andrei.bechet@esrlabs.com>
2015-10-25 12:11:35 +0530

    GREEN: use enum for the direction instead of string

--- src/main/scala-2.11/MarsRoverExpedition.scala
+++ src/main/scala-2.11/MarsRoverExpedition.scala
@@ -1,17 +1,19 @@
-class MarsRoverExpedition(var position: (Int, Int), val currentDirection: String) {
+import Direction.Direction
+
+class MarsRoverExpedition(var position: (Int, Int), val currentDirection: Direction) {
 
   def move(command: String) = {
     if (command == "f")
       currentDirection match {
-        case "N" => position = (position._1, position._2 + 1)
-        case "S" => position = (position._1, position._2 - 1)
-        case "E" => position = (position._1 + 1, position._2)
-        case "W" => position = (position._1 - 1, position._2)
+        case Direction.NORTH => position = (position._1, position._2 + 1)
+        case Direction.SOUTH => position = (position._1, position._2 - 1)
+        case Direction.EAST => position = (position._1 + 1, position._2)
+        case Direction.WEST => position = (position._1 - 1, position._2)
       }
   }
 
   def location(): (Int, Int) = position
 
-  def direction(): String = currentDirection
+  def direction(): Direction = currentDirection
 
 }
