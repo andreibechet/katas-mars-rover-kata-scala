86c43135e319047de06b2d8695622b3663ecef16
Andrei Bechet <andrei.bechet@esrlabs.com>
2015-10-25 12:07:25 +0530

    RED: use enum for the direction instead of string

---
+++ src/main/scala-2.11/Direction.scala
@@ -0,0 +1,4 @@
+object Direction extends Enumeration {
+  type Direction = Value
+  val NORTH, EAST, SOUTH, WEST = Value
+}
--- src/test/scala-2.11/TestMarsRoverExpedition.scala
+++ src/test/scala-2.11/TestMarsRoverExpedition.scala
@@ -3,31 +3,31 @@ import org.scalatest.{FlatSpec, Matchers}
 class TestMarsRoverExpedition extends FlatSpec with Matchers {
 
   "A Mars rover expedition" should " have an initial position and direction" in {
-    val marsRoverExpedition = new MarsRoverExpedition((1, 1), "N")
+    val marsRoverExpedition = new MarsRoverExpedition((1, 1), Direction.NORTH)
     marsRoverExpedition.location() should equal (1, 1)
-    marsRoverExpedition.direction() should equal ("N")
+    marsRoverExpedition.direction() should equal (Direction.NORTH)
   }
 
   "A Mars rovers position" should " increase by one in the existing direction when receiving the command f" in {
-    val northHeadingMarsRoverExpedition = new MarsRoverExpedition((2, 2), "N")
+    val northHeadingMarsRoverExpedition = new MarsRoverExpedition((2, 2), Direction.NORTH)
     northHeadingMarsRoverExpedition.move("f")
     northHeadingMarsRoverExpedition.location() should equal (2, 3)
-    northHeadingMarsRoverExpedition.direction() should equal ("N")
+    northHeadingMarsRoverExpedition.direction() should equal (Direction.NORTH)
 
-    val southHeadingMarsRoverExpedition = new MarsRoverExpedition((2, 2), "S")
+    val southHeadingMarsRoverExpedition = new MarsRoverExpedition((2, 2), Direction.SOUTH)
     southHeadingMarsRoverExpedition.move("f")
     southHeadingMarsRoverExpedition.location() should equal (2, 1)
-    southHeadingMarsRoverExpedition.direction() should equal ("S")
+    southHeadingMarsRoverExpedition.direction() should equal (Direction.SOUTH)
 
-    val eastHeadingMarsRoverExpedition = new MarsRoverExpedition((2, 2), "E")
+    val eastHeadingMarsRoverExpedition = new MarsRoverExpedition((2, 2), Direction.EAST)
     eastHeadingMarsRoverExpedition.move("f")
     eastHeadingMarsRoverExpedition.location() should equal (3, 2)
-    eastHeadingMarsRoverExpedition.direction() should equal ("E")
+    eastHeadingMarsRoverExpedition.direction() should equal (Direction.EAST)
 
-    val westHeadingMarsRoverExpedition = new MarsRoverExpedition((2, 2), "W")
+    val westHeadingMarsRoverExpedition = new MarsRoverExpedition((2, 2), Direction.WEST)
     westHeadingMarsRoverExpedition.move("f")
     westHeadingMarsRoverExpedition.location() should equal (1, 2)
-    westHeadingMarsRoverExpedition.direction() should equal ("W")
+    westHeadingMarsRoverExpedition.direction() should equal (Direction.WEST)
   }
 
 
