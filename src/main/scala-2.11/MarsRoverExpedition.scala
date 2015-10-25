2980a1e58b4390f4595b8a2f2ce060b37899c0f0
Andrei Bechet <andrei.bechet@esrlabs.com>
2015-10-25 11:32:50 +0530

    GREEN: A Mars rovers position should increase by one in ANY direction when receiving the command "f" (while maintaining the direction) - tests for all possible directions

--- src/main/scala-2.11/MarsRoverExpedition.scala
+++ src/main/scala-2.11/MarsRoverExpedition.scala
@@ -2,10 +2,12 @@ class MarsRoverExpedition(var position: (Int, Int), val currentDirection: String
 
   def move(command: String) = {
     if (command == "f")
-      if (currentDirection == "N")
-        position = (position._1, position._2 + 1)
-      else
-        position = (position._1, position._2 - 1)
+      currentDirection match {
+        case "N" => position = (position._1, position._2 + 1)
+        case "S" => position = (position._1, position._2 - 1)
+        case "E" => position = (position._1 + 1, position._2)
+        case "W" => position = (position._1 - 1, position._2)
+      }
   }
 
   def location(): (Int, Int) = position
--- src/test/scala-2.11/TestMarsRoverExpedition.scala
+++ src/test/scala-2.11/TestMarsRoverExpedition.scala
@@ -9,15 +9,26 @@ class TestMarsRoverExpedition extends FlatSpec with Matchers {
   }
 
   "A Mars rovers position" should " increase by one in the existing direction when receiving the command f" in {
-    val marsRoverExpedition = new MarsRoverExpedition((1, 1), "N")
-    marsRoverExpedition.move("f")
-    marsRoverExpedition.location() should equal (1, 2)
-    marsRoverExpedition.direction() should equal ("N")
+    val northHeadingMarsRoverExpedition = new MarsRoverExpedition((2, 2), "N")
+    northHeadingMarsRoverExpedition.move("f")
+    northHeadingMarsRoverExpedition.location() should equal (2, 3)
+    northHeadingMarsRoverExpedition.direction() should equal ("N")
 
-    val secondMarsRoverExpedition = new MarsRoverExpedition((2, 2), "S")
-    secondMarsRoverExpedition.move("f")
-    secondMarsRoverExpedition.location() should equal (2, 1)
-    secondMarsRoverExpedition.direction() should equal ("S")
+    val southHeadingMarsRoverExpedition = new MarsRoverExpedition((2, 2), "S")
+    southHeadingMarsRoverExpedition.move("f")
+    southHeadingMarsRoverExpedition.location() should equal (2, 1)
+    southHeadingMarsRoverExpedition.direction() should equal ("S")
+
+    val eastHeadingMarsRoverExpedition = new MarsRoverExpedition((2, 2), "E")
+    eastHeadingMarsRoverExpedition.move("f")
+    eastHeadingMarsRoverExpedition.location() should equal (3, 2)
+    eastHeadingMarsRoverExpedition.direction() should equal ("E")
+
+    val westHeadingMarsRoverExpedition = new MarsRoverExpedition((2, 2), "W")
+    westHeadingMarsRoverExpedition.move("f")
+    westHeadingMarsRoverExpedition.location() should equal (1, 2)
+    westHeadingMarsRoverExpedition.direction() should equal ("W")
   }
+
 
 }
