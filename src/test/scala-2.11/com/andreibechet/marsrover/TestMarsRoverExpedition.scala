6b9e439b91d6def9ea1b885bceed70d141a7c918
Andrei Bechet <andrei.bechet@esrlabs.com>
2015-10-25 14:17:57 +0530

    Refactor the tests:  - new matcher which checks both Coordinate and Direction (performs one check instead of one)

--- src/test/scala-2.11/com/andreibechet/marsrover/TestMarsRoverExpedition.scala
+++ src/test/scala-2.11/com/andreibechet/marsrover/TestMarsRoverExpedition.scala
@@ -5,45 +5,41 @@ import org.scalatest.{Assertion, FlatSpec, Matchers}
 
 class TestMarsRoverExpedition extends FlatSpec with Matchers {
 
-  class CoordinatesAreEqual(right: Coordinate) extends Matcher[Coordinate]{
-    def apply (left : Coordinate) : MatchResult =
-      MatchResult(left === right, "The compared coordinates are not equal", "The compared coordinates are equal" )
+  class ExpeditionsAreEqualMatcher(right: MarsRoverExpedition) extends Matcher[MarsRoverExpedition] {
+    def apply (left : MarsRoverExpedition) : MatchResult =
+      MatchResult(left.direction() === right.direction() && left.location() === right.location(),
+        "The compared expeditions are not equal", "The compared expeditions are equal" )
   }
-  def equalTheCoordinate(right: Coordinate) = new CoordinatesAreEqual(right)
+  def beTheSameAs(right: MarsRoverExpedition) = new ExpeditionsAreEqualMatcher(right)
 
   "A Mars rover expedition" should " have an initial position and direction" in {
     val marsRoverExpedition = new MarsRoverExpedition(Coordinate(1, 1), Direction.NORTH)
-    marsRoverExpedition.location() should equalTheCoordinate (Coordinate(1, 1))
-    marsRoverExpedition.direction() should equal (Direction.NORTH)
+    marsRoverExpedition should beTheSameAs(new MarsRoverExpedition(Coordinate(1, 1), Direction.NORTH))
   }
 
   "A Mars rovers position" should " increase by one in the existing direction when receiving the command f" in {
     def checkIfTheRoverIsFacingNorth: Assertion = {
-      val northHeadingMarsRoverExpedition = new MarsRoverExpedition(Coordinate(2, 2), Direction.NORTH)
-      northHeadingMarsRoverExpedition.move("f")
-      northHeadingMarsRoverExpedition.location() should equalTheCoordinate(Coordinate(2, 3))
-      northHeadingMarsRoverExpedition.direction() should equal(Direction.NORTH)
+      val expedition = new MarsRoverExpedition(Coordinate(2, 2), Direction.NORTH)
+      expedition.move("f")
+      expedition should beTheSameAs(new MarsRoverExpedition(Coordinate(2, 3), Direction.NORTH))
     }
 
     def checkIfTheRoverIsFacingSouth: Assertion = {
-      val southHeadingMarsRoverExpedition = new MarsRoverExpedition(Coordinate(2, 2), Direction.SOUTH)
-      southHeadingMarsRoverExpedition.move("f")
-      southHeadingMarsRoverExpedition.location() should equalTheCoordinate(Coordinate(2, 1))
-      southHeadingMarsRoverExpedition.direction() should equal(Direction.SOUTH)
+      val expedition = new MarsRoverExpedition(Coordinate(2, 2), Direction.SOUTH)
+      expedition.move("f")
+      expedition should beTheSameAs(new MarsRoverExpedition(Coordinate(2, 1), Direction.SOUTH))
     }
 
     def checkIfTheRoverIsFacingEast: Assertion = {
-      val eastHeadingMarsRoverExpedition = new MarsRoverExpedition(Coordinate(2, 2), Direction.EAST)
-      eastHeadingMarsRoverExpedition.move("f")
-      eastHeadingMarsRoverExpedition.location() should equalTheCoordinate(Coordinate(3, 2))
-      eastHeadingMarsRoverExpedition.direction() should equal(Direction.EAST)
+      val expedition = new MarsRoverExpedition(Coordinate(2, 2), Direction.EAST)
+      expedition.move("f")
+      expedition should beTheSameAs(new MarsRoverExpedition(Coordinate(3, 2), Direction.EAST))
     }
 
     def checkIfTheRoverIsFacingWest: Assertion = {
-      val westHeadingMarsRoverExpedition = new MarsRoverExpedition(Coordinate(2, 2), Direction.WEST)
-      westHeadingMarsRoverExpedition.move("f")
-      westHeadingMarsRoverExpedition.location() should equalTheCoordinate(Coordinate(1, 2))
-      westHeadingMarsRoverExpedition.direction() should equal(Direction.WEST)
+      val expedition = new MarsRoverExpedition(Coordinate(2, 2), Direction.WEST)
+      expedition.move("f")
+      expedition should beTheSameAs(new MarsRoverExpedition(Coordinate(1, 2), Direction.WEST))
     }
 
     checkIfTheRoverIsFacingNorth
