dad5ec61ef1238d1b1747aa35daa9dd1787d053f
Andrei Bechet <andrei.bechet@esrlabs.com>
2015-10-25 11:19:33 +0530

    GREEN: A Mars rovers position should increase by one in the existing direction when receiving the command "f" (while maintaining the direction) - tests for more given directions

--- src/main/scala-2.11/MarsRoverExpedition.scala
+++ src/main/scala-2.11/MarsRoverExpedition.scala
@@ -1,8 +1,11 @@
 class MarsRoverExpedition(var position: (Int, Int), val currentDirection: String) {
 
   def move(command: String) = {
-    if (command == "f") 
-      position = (position._1, position._2 + 1)
+    if (command == "f")
+      if (currentDirection == "N")
+        position = (position._1, position._2 + 1)
+      else
+        position = (position._1, position._2 - 1)
   }
 
   def location(): (Int, Int) = position
