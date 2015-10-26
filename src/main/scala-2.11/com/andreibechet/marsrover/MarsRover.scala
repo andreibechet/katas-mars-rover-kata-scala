c3f6c1832b9a353863541c83090aaffc9531933d
Andrei Bechet <andrei.bechet@esrlabs.com>
2015-10-26 13:24:34 +0530

    GREEN: test a more functional programming implementation of MarsRover  - a MarsRover object is immutable:     - the execute, turnLeft/Right, moveForward/Backward return a new MarRover instance     - the position and direction members are immutable

--- src/main/scala-2.11/com/andreibechet/marsrover/MarsRover.scala
+++ src/main/scala-2.11/com/andreibechet/marsrover/MarsRover.scala
@@ -4,8 +4,8 @@ import com.andreibechet.marsrover.Direction.Direction
 
 class MarsRover(initialPosition: Coordinate, initialDirection: Direction, theGrid: Grid) {
 
-  var position = initialPosition
-  var direction = initialDirection
+  val position = initialPosition
+  val direction = initialDirection
   val processCommands = new ProcessCommands()
   val computeTranslationVector = new ComputeTranslationVector()
   val grid = theGrid
@@ -14,44 +14,49 @@ class MarsRover(initialPosition: Coordinate, initialDirection: Direction, theGri
     this(initialPosition, initialDirection, new Grid(10, 10))
   }
 
-  def execute(commands: Array[Char]) = {
-    processCommands(commands)
+  def execute(commands: Array[Char]): MarsRover = {
+    processCommands(this, commands)
   }
 
-  def turnRight() = {
-    direction = direction match {
+  def turnRight(): MarsRover = {
+    val newDirection = direction match {
       case Direction.NORTH => Direction.EAST
       case Direction.SOUTH => Direction.WEST
       case Direction.EAST => Direction.SOUTH
       case Direction.WEST => Direction.NORTH
     }
+    new MarsRover(position, newDirection)
   }
 
-  def turnLeft() = {
-    direction = direction match {
+  def turnLeft(): MarsRover = {
+    val newDirection = direction match {
       case Direction.NORTH => Direction.WEST
       case Direction.SOUTH => Direction.EAST
       case Direction.EAST => Direction.NORTH
       case Direction.WEST => Direction.SOUTH
     }
+    new MarsRover(position, newDirection)
   }
 
-  def moveBackward() = {
+  def moveBackward(): MarsRover = {
     val movingForward = false
-    position = grid.moveFromWith(position, computeTranslationVector(movingForward, direction))
+    val newPosition = grid.moveFromWith(position, computeTranslationVector(movingForward, direction))
+    new MarsRover(newPosition, direction)
   }
 
-  def moveForward() = {
+  def moveForward(): MarsRover = {
     val movingForward = true
-    position = grid.moveFromWith(position, computeTranslationVector(movingForward, direction))
+    val newPosition = grid.moveFromWith(position, computeTranslationVector(movingForward, direction))
+    new MarsRover(newPosition, direction)
   }
 
   class ProcessCommands() {
-    def apply(commands: Array[Char]) = {
-      commands.foreach(processCommand)
+    def apply(marsRover: MarsRover, commands: Array[Char]): MarsRover = {
+      if (commands.isEmpty) return marsRover
+      apply(processCommand(commands.head), commands.tail)
     }
 
-    def processCommand(command: Char) = command match {
+    def processCommand(command: Char): MarsRover = command match {
       case 'f' => moveForward()
       case 'b' => moveBackward()
       case 'l' => turnLeft()
